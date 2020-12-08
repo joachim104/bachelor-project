26bdb9f2215b995edfd93123c89d472afac9ab34
Jens Backvall <jens@entropyfox.com>
2020-12-08 14:09:30 +0100

    add routing and startpage html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanethuntInventoryComponent } from './planethunt-inventory/planethunt-inventory.component';
+import { PlanethuntStartpageComponent } from './planethunt-startpage/planethunt-startpage.component';
+import { PlanetHuntTutorialComponent } from './planethunt-tutorial/planethunt-tutorial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/startpag', pathMatch: 'full' },
+  { path: 'startpage', component: PlanethuntStartpageComponent},
+  { path: 'tutorial', component:  PlanetHuntTutorialComponent },
+  { path: 'inventory', component: PlanethuntInventoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
