eec09e53e1c944d7c39c96487338992cbc88c979
Jens Backvall <jens@entropyfox.com>
2021-01-21 13:37:15 +0100

    add gzip compression and build with aot

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const compression = require('compression');
 const express = require('express');
 const app = express();
 
 app.use(express.static('./dist/brorfelde'));
+
+// Compress all HTTP responses
+app.use(compression());
 
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/brorfelde/'}
