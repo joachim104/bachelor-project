8cd50359552b3dd201fe3f612974935d71f76611
Jens Backvall <jens@entropyfox.com>
2021-02-02 11:14:32 +0100

    remove error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, config: {onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
