2147a0f76e38d60abbde8e22fea4e67e4cd3486a
Jens <jens@entropyfox.com>
2021-03-28 20:37:57 +0200

    set urls to live api

--- src/app/planethunt-inventory/planethunt-inventory.component.ts
+++ src/app/planethunt-inventory/planethunt-inventory.component.ts
@@ -28,8 +28,8 @@ export class PlanethuntInventoryComponent implements OnInit {
 
   ngOnInit() {
     this.userId = window.localStorage.getItem('userId');
-    //this.baseUrl = `https://view.quest.hololink.io/`;
-    this.baseUrl = `https://0.0.0.0:8080/`
+    this.baseUrl = `https://view.quest.hololink.io/`;
+    //this.baseUrl = `https://0.0.0.0:8080/`
     this.userService.getUser(this.userId).subscribe((response) => {
       this.planetArray = response.planets;
       this.checkNumberOfPlanetsVisitedAndCalculatePoints();
